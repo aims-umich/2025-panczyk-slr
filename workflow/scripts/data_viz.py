0063f7edc7db3c3e0f7a98e58da31840757d1507
Nataly Panczyk <npanczyk@umich.edu>
2026-02-28 12:19:00 -0500

    imports full bibliography

--- workflow/scripts/data_viz.py
+++ workflow/scripts/data_viz.py
@@ -134,8 +134,19 @@ def get_stats(df):
     reg_considered = reg_counts.get('Yes', 0)
     reg_not_considered = reg_counts.get('No', 0)
 
+    print(df['Industry'].unique())
+    total_papers = df['Industry'].value_counts()
+    medical = total_papers.get('Medical', 0)
+    manufac = total_papers.get('Manufacturing', 0)
+    ethics = total_papers.get('Ethics', 0)
+    aviation = total_papers.get('Aviation', 0)
+    avs = total_papers.get('Autonomous Vehicles', 0)
+    nuclear = total_papers.get('Nuclear', 0)
+
     print(f'XAI Considered: {xai_considered}, Total: {xai_considered + xai_not_considered}, Ratio: {xai_considered/(xai_considered + xai_not_considered)}')
     print(f'Reg Considered: {reg_considered}, Total: {reg_considered + reg_not_considered}, Ratio: {reg_considered/(reg_considered + reg_not_considered)}')
+
+    print(f'Medical: {medical/72}, Manufacturing: {manufac/72}, Ethics: {ethics/72}, Aviation: {aviation/72}, AVs: {avs/72}, Nuclear: {nuclear/72}')
     return
 
 
@@ -144,9 +155,9 @@ if __name__=="__main__":
                  usecols=['AI Type - Intervention', 'Comparison', 'Country', 'Data Type', 'Date','Decision Making - (Intervention)', 'Deployed?', 'Feature Study?', 'Industry', 'Output Feature','Place', 'Published Year', 'QA Score', 'Regulatory Oversight Body (if any)', 'Safety Outcome', 'Study','Subcategory', 'Was explainability/interpretability considered?', 'XAI Type', 'Was regulation considered?']
                 )
 
-    plot_countries(df)
-    plot_ai_type(df)
-    plot_explainability(df)
-    plot_DM_type(df)
-    plot_QA(df)
+    # plot_countries(df)
+    # plot_ai_type(df)
+    # plot_explainability(df)
+    # plot_DM_type(df)
+    # plot_QA(df)
     get_stats(df)
